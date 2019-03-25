36a4338794ce8965f9fcddae3a76f9295b230e79
elenaJ97 <eli.jakimovska@hotmail.com>
2019-03-25 19:14:56 +0100

    amaaaaan

--- EndToEnd/Models/Course.cs
+++ EndToEnd/Models/Course.cs
@@ -8,6 +8,7 @@ namespace EndToEnd.Models
 {
     public class Course
     {
+        [Required(ErrorMessage = "Внесете име")]
         [Display(Name = "Име на предмет")]
         public string Name { get; set; }
 
@@ -15,12 +16,17 @@ namespace EndToEnd.Models
         [Display(Name = "Код")]
         public int Code { get; set; }
 
+        [Required(ErrorMessage = "Може да внесете број од 1 до 8")]
+        [Range(1, 8, ErrorMessage = "Може да внесете број од 1 до 8")]
         [Display(Name = "Семестар")]
         public int Semester { get; set; }
 
+        [Required(ErrorMessage = "Внесете фонд на часови")]
         [Display(Name = "Фонд на часови")]
         public string Fond { get; set; }
 
+        [Required(ErrorMessage = "Може да внесете број од 3 до 10")]
+        [Range(1, 8, ErrorMessage = "Може да внесете број од 3 до 10")]
         [Display(Name = "Кредити")]
         public int Credits { get; set; }
 
